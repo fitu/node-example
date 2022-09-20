3450f153cfc46fed64c4533f8b288448a91bff0f
Victorio Matteucci <victorio.matteucci@gmail.com>
2022-09-20 19:18:06 -0300

    [fix/csv]: fix csv population

--- src/routines/src/shared/db/seeder.ts
+++ src/routines/src/shared/db/seeder.ts
@@ -2,7 +2,7 @@ import RoutineService from "modules/routine/domain/RoutineService";
 import { DbType } from "@shared/db/database";
 import InMemorySeeder from "@shared/db/inMemory/InMemorySeeder";
 
-const ROUTINES_CSV_PATH = "src/modules/user/infrastructure/data/routines.csv";
+const ROUTINES_CSV_PATH = "src/modules/routine/infrastructure/data/routines.csv";
 
 interface Seeder {
     seed: () => Promise<void>;
